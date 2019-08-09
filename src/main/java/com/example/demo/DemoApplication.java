a2e1f0fa5e4778f299db1957782386e94180a2ba
goodBoy <775134046@qq.com>
2019-08-10 00:19:25 +0800

    对启动类做了修改

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -9,7 +9,7 @@ public class DemoApplication {
     public static void main(String[] args) {
 
         SpringApplication.run(DemoApplication.class, args);
-        System.out.println("你是从哪里来的妖怪，我们明天早上八点钟左右就要去政和了！");
+        System.out.println("我吧打印内容修改了一下，测试git提交一下");
     }
 
 }
