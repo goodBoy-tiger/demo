c56f86918a2df5f3ae72eb4cc5b4bde90e3ac8cb
goodBoy <775134046@qq.com>
2020-08-19 22:05:30 +0800

    新弄了一个分支2

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -11,6 +11,7 @@ public class DemoApplication {
         SpringApplication.run(DemoApplication.class, args);
         System.out.println("我把刚刚创建的dev分支打印内容改了！");
         System.out.println("nihojj");
+        System.out.println("又弄了一个分支");
     }
 
 }
